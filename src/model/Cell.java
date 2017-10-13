d0a6a3b6c0c45846f6055d134bb7717b718d3757
Kevin GUOI <kevin.guoi57@gmail.com>
2017-10-13 16:30:12 +0200

    Create tests and corresponding methods in Cell.class

--- src/model/Cell.java
+++ src/model/Cell.java
@@ -4,40 +4,70 @@ public class Cell {
 
 	private Coord coord;
 	private boolean alive;
+	private int nbNeighbor;
 	
 	public Cell(int x, int y) {
 		this.coord = new Coord(x,y);
 		this.alive = true;
+		this.nbNeighbor = 0;
 	}
 
 	public boolean IsNeighbor(Cell cell1) {
-		boolean neighbor = false;
-		if((this.getCoord().getX() + 1 == cell1.getCoord().getX() 
-				&& this.getCoord().getY() == cell1.getCoord().getY())
-			|| (this.getCoord().getX() - 1 == cell1.getCoord().getX()
-				&& this.getCoord().getY() == cell1.getCoord().getY())
-			|| (this.getCoord().getY() + 1 == cell1.getCoord().getY()
-				&& this.getCoord().getX()==cell1.getCoord().getX())
-			|| this.getCoord().getY() - 1 == cell1.getCoord().getY()
-				&& this.getCoord().getX() == cell1.getCoord().getX()) {
-			neighbor = true;
-		}
-		return neighbor;
+		int neighbor = Math.abs(this.getCoord().getX() - cell1.getCoord().getX()) 
+				+ Math.abs(this.getCoord().getY() - cell1.getCoord().getY());
+		return neighbor == 1;
 	}
 
 	public boolean isAlive() {
-		return alive;
+		this.alive = moreThanOneNeighbor()&&lessThanFourNeighbor();
+		return this.alive;
 	}
 
 	public Coord getCoord() {
 		return coord;
 	}
-
-	public void setCoord(Coord coord) {
-		this.coord = coord;
-	}
-
+	
 	public void setAlive(boolean alive) {
 		this.alive = alive;
+	}
+
+	public int getNbNeighbor() {
+		return nbNeighbor;
+	}
+
+	public void setNbNeighbor(int nbNeighbor) {
+		this.nbNeighbor = nbNeighbor;
+	}
+	
+	public boolean moreThanOneNeighbor() {
+		return this.nbNeighbor > 1;
+	}
+	
+	public boolean lessThanFourNeighbor() {
+		return this.nbNeighbor < 4;
+	}
+	
+	public boolean revive() {
+		if(this.alive == false && this.nbNeighbor == 3) {
+			this.alive = true;
+		}
+		return this.alive;
+	}
+	
+	public int countNeighbor() {
+		this.nbNeighbor = 0;
+		if(new Cell(this.getCoord().getX() + 1,this.getCoord().getY()).isAlive()) {
+			this.nbNeighbor++;
+		}
+		if(new Cell(this.getCoord().getX() - 1,this.getCoord().getY()).isAlive()) {
+			this.nbNeighbor++;
+		}
+		if(new Cell(this.getCoord().getX(), this.getCoord().getY() + 1).isAlive()) {
+			this.nbNeighbor++;
+		}
+		if(new Cell(this.getCoord().getX(), this.getCoord().getY() - 1).isAlive()) {
+			this.nbNeighbor++;
+		}
+		return this.nbNeighbor;
 	}
 }
--- test/model/TestCell.java
+++ test/model/TestCell.java
@@ -27,10 +27,18 @@ public class TestCell {
 		Cell cell2 = new Cell(0,0);
 		Cell cell3 = new Cell(1,-1);
 		Cell cell4 = new Cell(2,0);
+		Cell cell5 = new Cell(2,1);
+		Cell cell6 = new Cell(0,1);
+		Cell cell7 = new Cell(2,-1);
+		Cell cell8 = new Cell(2,1);
 		assertTrue("NOK isAlive", cell.IsNeighbor(cell1));
 		assertTrue("NOK isAlive", cell.IsNeighbor(cell2));
 		assertTrue("NOK isAlive", cell.IsNeighbor(cell3));
 		assertTrue("NOK isAlive", cell.IsNeighbor(cell4));
+		assertFalse("NOK isAlive", cell.IsNeighbor(cell5));
+		assertFalse("NOK isAlive", cell.IsNeighbor(cell6));
+		assertFalse("NOK isAlive", cell.IsNeighbor(cell7));
+		assertFalse("NOK isAlive", cell.IsNeighbor(cell8));
 	}
 
 	@Test
@@ -41,13 +49,68 @@ public class TestCell {
 
 	@Test
 	public void testIsAlive() {
+		cell.setNbNeighbor(2);
 		assertTrue("NOK isAlive", cell.isAlive());
+		cell.setNbNeighbor(1);
+		assertFalse("NOK isAlive", cell.isAlive());
+		cell.setNbNeighbor(5);
+		assertFalse("NOK isAlive", cell.isAlive());
 	}
 
 	@Test
 	public void testIsAliveFalse() {
-		deadCell.setAlive(false);
+		deadCell.isAlive();
 		assertFalse("NOK isAlive", deadCell.isAlive());
 	}
 
+	@Test
+	public void testSetNbNeighbor() {
+		cell.setNbNeighbor(3);
+		assertEquals("NOK setNbNeighbor", 3, cell.getNbNeighbor());
+	}
+
+	@Test
+	public void testMoreThanOneNeighbor() {
+		cell.setNbNeighbor(2);
+		assertTrue("NOK moreThanOneNeighbor", cell.moreThanOneNeighbor());
+	}
+
+	@Test
+	public void testMoreThanOneNeighborFalse() {
+		cell.setNbNeighbor(1);
+		assertFalse("NOK moreThanOneNeighbor", cell.moreThanOneNeighbor());
+	}
+
+	@Test
+	public void testLessThanFourNeighbor() {
+		cell.setNbNeighbor(3);
+		assertTrue("NOK lessThanFourNeighbor", cell.lessThanFourNeighbor());
+	}
+
+	@Test
+	public void testLessThanFourNeighborFalse() {
+		cell.setNbNeighbor(5);
+		assertFalse("NOK lessThanFourNeighbor", cell.lessThanFourNeighbor());
+	}
+
+	@Test
+	public void testRevive() {
+		cell.setAlive(true);
+		cell.setNbNeighbor(3);
+		assertTrue("NOK revive", cell.revive());
+		
+		cell.setAlive(false);
+		cell.setNbNeighbor(2);
+		assertFalse("NOK revive", cell.revive());
+		cell.setNbNeighbor(3);
+		assertTrue("NOK revive", cell.revive());
+	}
+
+	@Test
+	public void testCountNeighbor() {
+		assertEquals("NOK countNeighbor", 0, cell.countNeighbor());
+		//TODO Set cell so that there are 4 neighbors
+		//assertEquals("NOK countNeighbor", 4, cell.countNeighbor());
+	}
+	
 }
